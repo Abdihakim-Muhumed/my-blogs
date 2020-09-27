2c267fa3b2784a977676e7ad3093ba012895911a
“Abdihakim <abdihakim.0017@gmail.com.comgit config --global user.name abdihakim17git config --global user.email abdihakim.0017@gmail.com.com>
2020-09-27 12:43:35 +0300

    Installed flask-SQLAlchemy

--- app/__init.py
+++ app/__init.py
@@ -3,7 +3,7 @@ def create_app(config_name):
     app = Flask(__name__)
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
-    
+
     #registering main blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
