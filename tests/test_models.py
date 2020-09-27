a614ca61461b82ae2d077ac4e081a9419ad4ef61
“Abdihakim <abdihakim.0017@gmail.com.comgit config --global user.name abdihakim17git config --global user.email abdihakim.0017@gmail.com.com>
2020-09-27 14:56:16 +0300

    Added testUser unittest

---
+++ tests/test_models.py
@@ -0,0 +1,17 @@
+import unittest
+from app.models import User, Blog, Comment
+
+class TestUser(unittest.TestCase):
+    '''class to test behaviour of User Model'''
+    def setUp(self):
+        self.new_user = User(password = 'Abdi9999')
+
+    def test_password_setter(self):
+        self.assertTrue(self.new_user.pass_secure is not None)
+
+    def test_no_accessing_password(self):
+        with self.assertRaises(AttributeError):
+            self.new_user.password
+
+    def test_password_verification(self):
+        self.assertTrue(self.new_user.verify_password('Abdi999'))
